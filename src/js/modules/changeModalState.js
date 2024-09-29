5d338456cc692556ccd8493d397a1e569010cf42
MariiaSimakova <mariiasimakova6@gmail.com>
2024-09-29 16:53:38 +1000

    add calc form validation

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -7,29 +7,45 @@ const changeModalState = (state) => {
     windowType = document.querySelectorAll("#view_type"),
     windowProfile = document.querySelectorAll(".checkbox");
 
+  const nextCalcBtn = document.querySelector(".calc_next_btn"),
+    nextNextB = document.querySelector(".popup_calc_profile_button");
+
   checkNumImputs("#width");
   checkNumImputs("#height");
 
   function bindActionToElems(event, elem, prop) {
+    nextCalcBtn.disabled = true;
+    nextProfileBtn.disabled = true;
+
     elem.forEach((item, i) => {
       item.addEventListener(event, () => {
         switch (item.nodeName) {
           case "SPAN":
             state[prop] = i;
             break;
+
           case "INPUT":
             if (item.getAttribute("type") === "checkbox") {
-              i === 0 ? (state[prop] = "Холодное") : (state[prop] = "Теплое");
-              elem.forEach((box, j) => {
-                box.checked = false;
-                if (i == j) {
-                  box.checked = true;
-                }
-              });
-            } else {
+              if (item.checked) {
+                i === 0 ? (state[prop] = "Холодное") : (state[prop] = "Теплое");
+                nextProfileBtn.removeAttribute("disabled");
+                elem.forEach((box, j) => {
+                  box.checked = false;
+                  if (i == j) {
+                    box.checked = true;
+                  }
+                });
+              }
+            }
+
+            if (item.getAttribute("type") === "text") {
               state[prop] = item.value;
+              if (state.width && state.height) {
+                nextCalcBtn.removeAttribute("disabled");
+              }
             }
             break;
+
           case "SELECT":
             state[prop] = item.value;
             break;
